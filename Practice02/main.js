166379755dd853e5fd866abc0f0f46c38c2bf07e
Jongkwan Choi <doemqjffns97@naver.com>
2022-07-02 23:53:48 +0900

    07.02

---
+++ Practice02/main.js
@@ -0,0 +1,32 @@
+    const animals = [
+        {name:'사자', size:'big'},
+        {name:'개', size:'small'},
+        {name:'닭', size:'small'},
+    ]
+console.log(animals)
+
+const num= 9;
+
+// ==은 데이터탑은은 비교하지않지만 ===은 데이터 타입까지 비교한다
+
+if(num ===10){
+    console.log('10입니다')
+}else if(num <10){
+    console.log('10보다 작습니다')
+}else {
+    console.log('10보다 큽니다.')
+}
+
+const animal = 'dog'
+
+
+switch(animal){
+    case 'lion':
+        console.log('사자입니다');
+        break;
+    case 'dog':
+        
+
+
+}
+console.log('사자입니다');
